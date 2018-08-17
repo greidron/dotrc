405bb242b6e8f9008264d37bc6f55156fc70314e
greidron <greidron@gmail.com>
2018-08-17 11:30:49 +0900

    add tmux configuration.

---
+++ install_tmux.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+
+DOTRC_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+# install .tmux.conf
+sed "s|@DOTRC_ROOT@|$DOTRC_ROOT|g" $DOTRC_ROOT/tmux/tmux.conf > ~/.tmux.conf
+
+# bootstraping.
+tmux new "echo 'bootstrapping tmux ...' && tmux source ~/.tmux.conf"
+
+echo "Installed tmux ;)"
