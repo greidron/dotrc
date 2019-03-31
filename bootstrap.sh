36081bcf4c89b606a8eeecc6c5e459de613d9251
greidron <greidron@gmail.com>
2019-03-31 09:20:44 +0000

    bootstrap.sh: use curl instead of wget.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,11 +2,10 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 # get pacapt
-wget https://github.com/icy/pacapt/raw/ng/pacapt -O ${SCRIPT_DIR}/pacapt
-chmod +x ${SCRIPT_DIR}/pacapt
+curl -sL https://github.com/icy/pacapt/raw/ng/pacapt -o ${SCRIPT_DIR}/pacapt
 # install packages.
 PACKAGES="cmake make gcc python-devel python-dev ncurses-devel ncurses-dev git tig"
 for PACKAGE in $PACKAGES; do
-    ${SCRIPT_DIR}/pacapt install --noconfirm ${PACKAGE}
+    ${SHELL} ${SCRIPT_DIR}/pacapt install --noconfirm ${PACKAGE}
 done
 rm -f ${SCRIPT_DIR}/pacapt
