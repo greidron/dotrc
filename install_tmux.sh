50e75cb7977c4c234147444f2b6c1467bfc0d55e
greidron <greidron@gmail.com>
2019-03-31 09:41:34 +0000

    Run tmux install plugin in install_tmux.sh

--- install_tmux.sh
+++ install_tmux.sh
@@ -8,4 +8,7 @@ sed "s|@DOTRC_ROOT@|$DOTRC_ROOT|g" $DOTRC_ROOT/tmux/tmux.conf > ~/.tmux.conf
 # bootstraping.
 tmux new "echo 'bootstrapping tmux ...' && tmux source ~/.tmux.conf"
 
+# install plugins.
+~/.tmux/plugins/tpm/bin/install_plugins
+
 echo "Installed tmux ;)"
